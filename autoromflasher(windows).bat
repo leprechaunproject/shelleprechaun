b2b9d00bb96e6f8d0a1894f62a0af711e5f6d610
LeprechaunProject <leprechaunproject@yahoo.com>
2015-02-21 01:42:45 -0300

    support for windows

---
+++ autoromflasher(windows).bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+echo "Auto ROM Flasher, by Luiz Nickel"
+fastboot flash partition gpt.bin
+fastboot flash motoboot motoboot.img
+fastboot flash logo logo.bin
+fastboot flash boot boot.img
+fastboot flash recovery recovery.img
+fastboot flash system system.img_sparsechunk.0
+fastboot flash system system.img_sparsechunk.1
+fastboot flash system system.img_sparsechunk.2
+fastboot flash system system.img_sparsechunk.3
+fastboot flash modem NON-HLOS.bin
+fastboot erase modemst1 
+fastboot erase modemst2 
+fastboot flash fsg fsg.mbn
+fastboot erase cache 
+fastboot erase userdata
+fastboot reboot
+echo "Thanks for using! XDA Username: mrnickel"
+pause
