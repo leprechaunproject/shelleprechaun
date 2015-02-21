d8f98d99ef553d2cc200567094ea12e734254ca0
LeprechaunProject <leprechaunproject@yahoo.com>
2015-02-21 02:10:23 -0300

    added some features like twrp and phiz support
    needs source file only downloadable on XDA post.

--- autoromflasher(linux).sh
+++ autoromflasher(linux).sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Auto ROM Flasher, by Luiz Nickel"
+echo "AutoRomFlasher! xda username: mrnickel"
 fastboot flash partition gpt.bin
 fastboot flash motoboot motoboot.img
 fastboot flash logo logo.bin
@@ -16,4 +16,38 @@ fastboot flash fsg fsg.mbn
 fastboot erase cache 
 fastboot erase userdata
 fastboot reboot
-echo "Thanks for using! XDA Username: mrnickel"
+echo "Some options..."
+echo "1 - Flash latest TWRP Package"
+echo "2 - Flash latest Philz Package"
+echo "R - Reboot device"
+echo
+echo -n "> "
+read choice
+echo
+recovery1 () {
+case "$1" in
+    boot)
+        $fastboot boot sources/$twrp.img
+        ;;
+    flash)
+        $fastboot flash recovery sources/$twrp.img
+        ;;
+esac
+}
+otherrecovery () {
+case "$2" in
+    boot)
+        $fastboot boot sources/philz.img
+        ;;
+    flash)
+        $fastboot flash recovery sources/philz.img
+        ;;
+esac
+}
+reboot () {
+case "$R" in
+		boot)
+			$fastboot reboot
+			;;
+esac
+}
