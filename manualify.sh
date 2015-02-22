d4447510d97133596eafc81ba8686bdf9b00a9a4
LeprechaunProject <leprechaunproject@yahoo.com>
2015-02-21 21:51:39 -0300

    root verify added / typo fixed
    experimental script.

--- manualify.sh
+++ manualify.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-echo "Este script executa o Spotify diretamente da linha de comando."
-su "am start -n com.spotify.music/com.spotify.music.MainActivity"
+#!/system/bin/sh
+# Teste de script - Manualify
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+# Verificar se o modo SU esta ativado.
+if [ "$(id -u)" != "0" ]; then
+   echo "Este script requer o su ativado!" 1>&2
+   exit 1
+fi
+am start -n com.spotify.music/com.spotify.music.MainActivity
