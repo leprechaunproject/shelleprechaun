c85b721baf061d0721f712161cba2d4b8efadd5e
LeprechaunProject <leprechaunproject@yahoo.com>
2015-02-21 23:07:57 -0300

    refurbished

--- Manualify/manualify.sh
+++ Manualify/manualify.sh
@@ -1,10 +1,17 @@
 #!/system/bin/sh
-# Teste de script - Manualify
-FILE="/tmp/out.$$"
-GREP="/bin/grep"
-# Verificar se o modo SU esta ativado.
-if [ "$(id -u)" != "0" ]; then
-   echo "Este script requer o su ativado!" 1>&2
-   exit 1
-fi
-am start -n com.spotify.music/com.spotify.music.MainActivity
+# Script principal - Manualify
+# Por Luiz Nickel e Pedro Franco @ Shelleprechaun
+echo "seu superusuario foi ativado?"
+sleep 2
+echo "caso funcionar, o superusuario foi configurado corretamente!"
+sleep 2
+echo "caso aparecer uma caralhada de erro em Java, o superusuario nao foi ativado ou voce nao o possui!"
+sleep 2
+echo "tentando spotify em 3 segundos"
+sleep 3
+   am start -n com.spotify.music/com.spotify.music.MainActivity
+echo "comando inserido."
+sleep 4
+echo "este comando apenas abre o spotify. irei abrir uma pagina com todos os scripts disponiveis para o manualify!"
+am start -a android.intent.action.VIEW -d https://github.com/leprechaunproject/shelleprechaun/tree/master/Manualify
+end
